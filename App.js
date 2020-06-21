a0cd250ab5b6784fe63c34dd658ca11d2f131811
Mukul Shakya <muklshakya123@gmail.com>
2020-06-21 08:46:17 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,5 +1,26 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import MainScreen from "./src/screens/mainScreen";
+import AppNavigator from "./src/navigation/appNavigator";
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
+
+// function SettingsScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//     </View>
+//   );
+// }
+
+// const Tab = createBottomTabNavigator();
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -7,30 +28,32 @@ const instructions = Platform.select({
 });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+  return <AppNavigator />;
+  // return (
+  //   <NavigationContainer>
+  //     <Tab.Navigator>
+  //       <Tab.Screen name="Home" component={HomeScreen} />
+  //       <Tab.Screen name="Settings" component={SettingsScreen} />
+  //     </Tab.Navigator>
+  //    </NavigationContainer>
+  // )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
 });
---
+++ ios/Pods/Target Support Files/Pods-chatreactnative/Pods-chatreactnative-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatreactnative : NSObject
+@end
+@implementation PodsDummy_Pods_chatreactnative
+@end
